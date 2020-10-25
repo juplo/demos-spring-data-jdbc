juplo/demos-kafka-polling-outbox

Simple web-app example for spring-boot-data-jdbc

src/main/java/de/juplo/boot/data/jdbc/UserRepository.java
@@ -1,0 +1,10 @@
+package de.juplo.boot.data.jdbc;
+
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    @Query("select * from User u where u.username = :username")
+    User findByUsername(@Param("username") String username);
+}

juplo/demos-spring-data-jdbc

src/main/java/de/juplo/boot/data/jdbc/UserRepository.java
@@ -1,0 +1,10 @@
+package de.juplo.boot.data.jdbc;
+
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    @Query("select * from users u where u.username = :username")
+    User findByUsername(@Param("username") String username);
+}